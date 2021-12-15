c781c262415de9fd6880957e7422e976765640ad
Michael Brook <michaelcbrook@gmail.com>
2021-12-14 21:07:53 -0600

    Updated warning notice

--- notice.js
+++ notice.js
@@ -1,12 +1,12 @@
 if (Package['accounts-ui']
     && !Package['service-configuration']
-    && !Object.prototype.hasOwnProperty.call(Package, 'pitchly-config-ui')) {
+    && !Object.prototype.hasOwnProperty.call(Package, 'pitchly:pitchly-config-ui')) {
   console.warn(
     "Note: You're using accounts-ui and accounts-pitchly,\n" +
     "but didn't install the configuration UI for the Pitchly\n" +
     "OAuth. You can install it with:\n" +
     "\n" +
-    "    meteor add pitchly-config-ui" +
+    "    meteor add pitchly:pitchly-config-ui" +
     "\n"
   );
 }
